1a6e98f0b9b8d4313953a025802f8fb004d77897
godjulius <duongnp28072003@gmail.com>
2022-09-23 12:44:29 +0700

    test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.example.bomberman_team7 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                                requires com.almasb.fxgl.all;
-    
+
+    requires com.almasb.fxgl.all;
+
     opens com.example.bomberman_team7 to javafx.fxml;
     exports com.example.bomberman_team7;
 }
