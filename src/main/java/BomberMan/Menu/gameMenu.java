5106e0fa14ba176380ee5ee55df00f5248db7725
godjulius <duongnp28072003@gmail.com>
2022-10-01 14:51:54 +0700

    Something by npd.28

---
+++ src/main/java/BomberMan/Menu/gameMenu.java
@@ -0,0 +1,12 @@
+package BomberMan.Menu;
+
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.MenuType;
+import org.jetbrains.annotations.NotNull;
+
+public class gameMenu extends FXGLMenu {
+
+    public gameMenu() {
+        super(MenuType.GAME_MENU);
+    }
+}
---
+++ src/main/java/BomberMan/Menu/mainMenu.java
@@ -0,0 +1,4 @@
+package BomberMan.Menu;
+
+public class mainMenu {
+}
