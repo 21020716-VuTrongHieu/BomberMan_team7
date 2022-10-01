eedea92ac18b2d7bd94879d3f786b5401c713e36
godjulius <duongnp28072003@gmail.com>
2022-10-01 14:21:18 +0700

    Something by npd.28

---
+++ src/main/java/BomberMan/constValue/constValue.java
@@ -0,0 +1,7 @@
+package BomberMan.constValue;
+
+// Lưu các giá trị hằng
+public class constValue {
+    public static final int GAME_WIDTH = 1280;
+    public static final int GAME_HEIGHT = 720;
+}
---
+++ src/main/java/BomberMan/gameMain.java
@@ -0,0 +1,16 @@
+package BomberMan;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class gameMain extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
