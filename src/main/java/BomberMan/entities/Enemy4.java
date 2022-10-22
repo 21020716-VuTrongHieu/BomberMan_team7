de356124c98d617408ce1ddad4b44aa8e6eb2102
Vu_Trong_Hieu <21020716@vnu.edu.vn>
2022-10-22 22:12:19 +0700

    add text and change load enemy

--- src/main/java/BomberMan/entities/Enemy4.java
+++ src/main/java/BomberMan/entities/Enemy4.java
@@ -56,7 +56,7 @@ public class Enemy4 extends Enemy {
         }
 
         if (state != State.DIE) calculateState();
-//            this.checkToMap();
+///            this.checkToMap();
     }
 
     @Override
