16d27c6a0bfb11445b0470af9690779cddee08c9
Vu_Trong_Hieu <21020716@vnu.edu.vn>
2022-10-22 22:11:39 +0700

    add text and change load enemy

--- src/main/java/BomberMan/entities/Enemy4.java
+++ src/main/java/BomberMan/entities/Enemy4.java
@@ -71,6 +71,8 @@ public class Enemy4 extends Enemy {
                 gc.drawImage(Sprite.mob_dead2.getFxImage(), this.getPosition().getX(), this.getPosition().getY(), constValue.ENTITY_SIZE, constValue.ENTITY_SIZE);
             } else if (frame >= 45 && frame < 60) {
                 gc.drawImage(Sprite.mob_dead3.getFxImage(), this.getPosition().getX(), this.getPosition().getY(), constValue.ENTITY_SIZE, constValue.ENTITY_SIZE);
+            } else if (frame == 60) {
+                constValue.ENEMIES--;
             }
             return;
         }
